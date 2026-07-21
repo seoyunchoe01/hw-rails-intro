157b9e294b5cb77119193ab967646ca004db166f
Adam Choe <seoyunchoe@berkeley.edu>
2026-07-21 03:19:57 +0000

    added filtering

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,16 @@ class MoviesController < ApplicationController
 
   # GET /movies or /movies.json
   def index
-    @movies = Movie.all
+    @all_ratings = Movie.all_ratings
+
+    if params[:ratings].present?
+      @ratings_to_show = params[:ratings].keys
+      @movies = Movie.with_ratings(@ratings_to_show)
+    else
+      @ratings_to_show = @all_ratings
+      @movies = Movie.all
+    end
+
   end
 
   # GET /movies/1 or /movies/1.json
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,9 @@
 class Movie < ApplicationRecord
+  def self.all_ratings
+    ['G', 'PG', 'PG-13', 'R']
+  end
+
+  def self.with_ratings(ratings)
+    where(rating: ratings)
+  end
 end
